5e520350670647e48275863f600e30eabcad4300
Corentin Duchêne <corentin.duchene@epita.fr>
2022-11-08 15:11:46 +0100

    feat: add logger

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
+func initLogger() *zap.Logger {
+	// Create a new logger with our custom configuration
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	config.DisableStacktrace = true // Disable stacktrace after WARN or ERROR
+
+	logger, _ := config.Build()
+
+	logger.Info("Logger initialized")
+	return logger
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	logger := initLogger()
+	defer logger.Sync()
+
+	logger.Info("Hello World")
 }
